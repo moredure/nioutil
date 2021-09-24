68650321b7cc9bbc02f8fce065e1ca83fa7b535a
Mikhail Faraponov <11322032+moredure@users.noreply.github.com>
2021-09-24 16:04:00 +0300

    Create nioutil.go

---
+++ nioutil.go
@@ -0,0 +1,30 @@
+package nioutil
+
+import "io"
+
+type DoubleReader struct {
+	reader  int
+	Readers [2]io.Reader
+}
+
+func (mr *DoubleReader) Read(p []byte) (n int, err error) {
+	for mr.reader < 2 {
+		n, err = mr.Readers[mr.reader].Read(p)
+		if err == io.EOF {
+			mr.reader += 1
+		}
+		if n > 0 || err != io.EOF {
+			if err == io.EOF && mr.reader < 2 {
+				err = nil
+			}
+			return
+		}
+	}
+	return 0, io.EOF
+}
+
+func (d *DoubleReader) Reset(first, second io.Reader) {
+  *d = DoubleReader{
+    Readers: [2]io.Reader{first, second}
+  }
+}
